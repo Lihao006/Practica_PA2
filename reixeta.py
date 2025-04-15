605f2fcb45fc8c9f9918938a748916ad6a1974dd
Lihao006 <lihao.jin@estudiantat.upc.edu>
2025-04-15 19:16:03 +0200

    Update reixeta.py

--- reixeta.py
+++ reixeta.py
@@ -2,7 +2,7 @@ from pytokr import item
 
 class Reixeta():
     # A completar pel grup d'estudiants com a part de la pràctica
-    def ___init__(self, n=0, k=0, ):
+    def __init__(self, n=0, k=0, ):
         assert (n > 0 and k > 0) or (n == 0 or k == 0), "Error: dimensions impossibles"
         self._num_fils = n
         self._num_cols = k
