85f14b027d264d262afccf20724f1c0e23ba94e5
bboscar248 <oscar.lu@estudiantat.upc.edu>
2025-05-22 17:06:46 +0200

    update

--- reixeta.py
+++ reixeta.py
@@ -93,10 +93,8 @@ class Reixeta():
 
         #Imprimim les posicions dels forats (0º, 90º, 180º, 270º)
         for rotacio in self._forats_rotacions: 
-            # print(" ".join(f"({i},{j})" for i, j in rotacio))
+            print(" ".join(f"({i},{j})" for i, j in rotacio))
 
-            for x, y in rotacio:
-                print(f"({x},{y})", end=' ')
 
 
 
